ffca4fbe7f239610a4ddc131286dc9f4ad8990d8
khermstad <krishermstad@gmail.com>
2017-09-29 01:09:34 -0400

    added port variable

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var path = require('path');
 
+var port = 3000;
+
 var app = express();
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
@@ -19,4 +21,4 @@ app.get('/catalog', function (req, res) {
     res.render('catalog');
 })
 
-app.listen(3000);
+app.listen(port, () => console.log("app.js running on port: " + port));
