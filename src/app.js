ee74b8289ab51f9fdc0eacdf82108741f2978c10
Kris Hermstad <krishermstad@gmail.com>
2017-09-29 21:16:03 -0400

    further restructuring

--- src/app.js
+++ src/app.js
@@ -4,13 +4,6 @@ const exphbs  = require('express-handlebars');
 const path = require('path');
 const app = express();
 
-// route imports
-const index = require("./routes/index.js")
-const catalog = require("./routes/catalog.js")
-const artists = require("./routes/artists.js")
-
-const port = 3000;
-
 // view settings
 app.engine('handlebars', exphbs({defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts'}));
 app.set('view engine', 'handlebars');
@@ -19,11 +12,16 @@ app.set('views', __dirname + '/views');
 // static public directory (for css/jss and other static files)
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
-app.listen(port, () => console.log("app.js running on port: " + port));
+// route imports
+const index = require("./routes/index.js")
+const catalog = require("./routes/catalog.js")
+const artists = require("./routes/artists.js")
 
 // routes
 app.use("/", index)
-
 app.use("/catalog", catalog)
-
 app.use("/artists", artists)
+
+const port = 3000;
+app.listen(port, () => console.log("app.js running on port: " + port));
+
