84caa09c3942ef0a0582fbab272dfaa7b1e6cd9c
Kris Hermstad <khermstad@users.noreply.github.com>
2017-10-10 21:15:31 -0400

    dev merge to master.  added login/register with sessions. (#4)
    * added .idea to .gitignore for any .idea projects
    
    * applied beautify
    
    * added register form
    
    * cleaned up register
    
    * added post route for register.js
    
    * updating post route
    
    * added semicolons
    
    * added sequelize to package.json
    
    * added sequelize connection, with dbconfig (not uploaded)
    
    * basic register functionality working
    
    * added error messages and routing for register
    
    * added password hashing via bcrypt
    
    * fixed success_error messages
    
    * reformatted file
    
    * tidy up
    
    * added login functionality
    
    * added basic login user check; todo password
    
    * basic login with proper password hashing
    
    * added login error messages
    
    * removed email tag
    
    * added userhome route
    
    * updated maxAge
    
    * added check for log in on userhome
    
    * added body-parser
    
    * added my account dropdown
    
    * updated dropdown-header
    
    * added mycatalog,mysettings, and uploadtrack

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,11 @@ const catalog = require("./routes/catalog.js");
 const artists = require("./routes/artists.js");
 const register = require("./routes/register.js");
 const login = require("./routes/login.js");
+const mycatalog = require("./routes/mycatalog.js");
+const uploadtrack = require("./routes/uploadtrack.js");
+const mysettings = require("./routes/mysettings.js");
 const userhome = require("./routes/userhome.js");
+
 
 // port settings
 const port = 5000;
@@ -41,7 +45,11 @@ app.use("/catalog", catalog);
 app.use("/artists", artists);
 app.use("/register", register);
 app.use("/login", login);
+app.use("/mycatalog", mycatalog)
+app.use("/uploadtrack", uploadtrack)
+app.use("/mysettings", mysettings)
 app.use('/userhome', userhome)
+
 
 app.get("/signout", (req, res) => {
     req.session.destroy(console.log('session destroyed'))
