2f753d3d743af153635bcb1f2000ffa5422ce35b
khermstad <krishermstad@gmail.com>
2017-10-12 09:26:17 -0400

    cleaned up old routes

--- src/js/routes/mycatalog.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => res.render("mycatalog", {email: req.session.email}))
-
-module.exports = router;
--- src/js/routes/uploadtrack.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => res.render("uploadtrack", {email: req.session.email}))
-
-module.exports = router;
