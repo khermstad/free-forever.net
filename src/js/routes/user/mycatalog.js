fa5f557966f8ff1b62d1c437a55813e84e0d2be1
Kris Hermstad <krishermstad@gmail.com>
2017-10-13 01:51:58 -0400

    input validation added using pattern inside input tag

--- src/js/routes/user/mycatalog.js
+++ src/js/routes/user/mycatalog.js
@@ -1,6 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const track = require('./../../models/Track')
+const db = require('../../db')
 
-router.get("/", (req, res) => res.render("user/mycatalog", {req: req}));
+router.get("/", (req, res) => {
+    getTracksByEmail(req.session.email, req, res)
+})
+
+const Track = db.define('track', track.schema, {
+    timestamps: true,
+    createdAt: 'created',
+    updatedAt: false
+})
+
+const getTracksByEmail = (email, req, res) => {
+    return Track.findAll({
+        where: {
+            email: email
+        }
+    })
+    .then(tracks => {
+        // console.log(tracks[0].dataValues.s3key)
+       for(let track of tracks){
+           track.s3link = `https://s3.amazonaws.com/ffm-datastore/${track.dataValues.s3key}`
+       }
+       res.render("user/mycatalog", {req: req, tracks: tracks})
+    })
+}
 
 module.exports = router;
