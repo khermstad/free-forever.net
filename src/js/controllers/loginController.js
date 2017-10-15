5e44c72839708b1335ebfdb170ed91ad360acde0
Kris Hermstad <krishermstad@gmail.com>
2017-10-15 02:07:02 -0400

    created loginController

---
+++ src/js/controllers/loginController.js
@@ -0,0 +1,61 @@
+const user = require('./../models/User')
+const db = require("./../db")
+const bcrypt = require('bcrypt-nodejs')
+const salt = bcrypt.genSaltSync(10);
+
+const User = db.define('user', user.schema, {
+    timestamps: false
+})
+
+const isValidEmail = (email) => {
+    return User.count({
+            where: {
+                email: email
+            }
+        })
+        .then(count => {
+            if (count === 1) {
+                return true;
+            }
+            return false;
+        })
+}
+
+const isValidPassword = (email, password) => {
+    return User.findOne({
+        where: {
+            email: email
+        }
+    })
+    .then(user =>{
+        const stored_password = user.dataValues.password
+        const isPassword = bcrypt.compareSync(password, stored_password)
+        return isPassword;
+    })
+    return false;
+}
+
+export const index = (req, res) => res.render('login', {req: req})
+
+export const login = (req, res) => {
+    const {email, password} = req.body;
+    
+        isValidEmail(email).then(isValid => {
+            if(isValid) {
+                isValidPassword(email, password).then(isValid =>{
+                    if (isValid){
+                        req.session.email = email;
+                        return res.render('user/userhome', {req: req})
+    
+                    }
+                    else{
+                        return res.render('login', {login_error_message: "Incorrect Password"})
+                    }
+                })
+            }
+            else{
+                return res.render('login', {login_error_message: "Unrecognized email."})
+            }
+        }) 
+        
+}
--- src/js/routes/login.js
+++ src/js/routes/login.js
@@ -1,65 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const user = require('../models/User.js')
-const db = require("../db")
-const bcrypt = require('bcrypt-nodejs')
-const salt = bcrypt.genSaltSync(10);
+const login_controller = require('./../controllers/loginController')
 
-const User = db.define('user', user.schema, {
-    timestamps: false
-})
-
-const isValidEmail = (email) => {
-    return User.count({
-            where: {
-                email: email
-            }
-        })
-        .then(count => {
-            if (count === 1) {
-                return true;
-            }
-            return false;
-        })
-}
-
-const isValidPassword = (email, password) => {
-    return User.findOne({
-        where: {
-            email: email
-        }
-    })
-    .then(user =>{
-        const stored_password = user.dataValues.password
-        const isPassword = bcrypt.compareSync(password, stored_password)
-        return isPassword;
-    })
-    return false;
-}
-
-router.get("/", (req, res) => res.render("login", {req: req}));
-
-router.post("/", (req, res) => {
-    const {email, password} = req.body;
-
-    isValidEmail(email).then(isValid => {
-        if(isValid) {
-            isValidPassword(email, password).then(isValid =>{
-                if (isValid){
-                    req.session.email = email;
-                    return res.render('user/userhome', {req: req})
-
-                }
-                else{
-                    return res.render('login', {login_error_message: "Incorrect Password"})
-                }
-            })
-        }
-        else{
-            return res.render('login', {login_error_message: "Unrecognized email."})
-        }
-    }) 
-    
-})
+router.get("/", login_controller.index);
+router.post("/", login_controller.login); 
 
 module.exports = router;
