ba74c2bb972fe425dddcbb48b8529c55649b5f2b
Kris Hermstad <krishermstad@gmail.com>
2017-10-15 00:49:19 -0400

    catalog now displays approved tracks

--- src/js/controllers/catalogController.js
+++ src/js/controllers/catalogController.js
@@ -1,1 +1,26 @@
-export const index = (req, res) => res.render("catalog", {req: req});
+//  /catalog controller
+const track = require("./../models/Track")
+const db = require('../db')
+
+// track schema
+const Track = db.define('track', track.schema, {
+    timestamps: true,
+    createdAt: 'created',
+    updatedAt: false
+})
+
+const getAllApprovedTracks = (req, res) => {
+    return Track.findAll({
+        where: {
+            approved: true
+        }
+    })
+    .then(tracks => {
+        for(let track of tracks){
+            track.s3link = `https://s3.amazonaws.com/ffm-datastore/${track.dataValues.s3key}`
+        }
+        res.render("catalog", {req: req, tracks: tracks})
+    })
+}
+
+export const index = (req, res) => getAllApprovedTracks(req, res);
