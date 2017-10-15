1ac3239a90d7ec06a7ae68bfe36bb41fe51f8fa2
Kris Hermstad <krishermstad@gmail.com>
2017-10-15 12:07:15 -0400

    created user/mycatalog controller

--- src/js/routes/user/mycatalog.js
+++ src/js/routes/user/mycatalog.js
@@ -1,33 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const track = require('./../../models/Track')
-const db = require('../../db')
+const mycatalog_controller = require('./../../controllers/user/mycatalogController')
 
-// Track schema for Sequelize
-const Track =  db.define('track', track.schema, {
-    timestamps: true,
-    createdAt: 'created',
-    updatedAt: false
-})
-
-router.get("/", (req, res) => {
-    getTracksByEmail(req.session.email, req, res)
-})
-
-const getTracksByEmail = (email, req, res) => {
-    return Track.findAll({
-        where: {
-            email: email
-        }
-    })
-    .then(tracks => {
-        // console.log(tracks[0].dataValues.s3key)
-       for(let track of tracks){
-           track.s3link = `https://s3.amazonaws.com/ffm-datastore/${track.dataValues.s3key}`
-       }
-       res.render("user/mycatalog", {req: req, tracks: tracks})
-    })
-}
-
+router.get("/", mycatalog_controller.index)
 
 module.exports = router;
