a19b4fa1449c7109e199adcc3acbe014c7f58bc6
Kris Hermstad <krishermstad@gmail.com>
2017-11-04 00:29:25 -0400

    artist page renders with artist name

--- src/js/controllers/artistsController.js
+++ src/js/controllers/artistsController.js
@@ -1,14 +1,37 @@
 // catalog controller
 const user = require('./../models/User')
 const db = require('./../db')
+const track = require("./../models/Track")
 
 // User model
 const User = db.define('user', user.schema, {
     timestamps: false
 })
 
+// track schema
+const Track = db.define('track', track.schema, {
+    timestamps: true,
+    createdAt: 'created',
+    updatedAt: false
+})
+
 export const index = (req, res) => {
     getAllArtists(req, res)
+}
+
+export const getArtist = (req, res) => {
+
+    return Track.findAll({
+        where: {
+            displayedname: req.params.displayedname
+        }
+    })
+        .then(tracks => {
+
+            res.render('artist', {tracks: tracks, artist: req.params.displayedname})
+        })
+
+
 }
 
 const getAllArtists = (req, res) => {
--- src/js/routes/artists.js
+++ src/js/routes/artists.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const artists_controller = require('./../controllers/artistsController')
 
 router.get("/", artists_controller.index);
+router.get("/:displayedname", artists_controller.getArtist)
 
 module.exports = router;
